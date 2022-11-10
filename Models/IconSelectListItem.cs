31da03b6da1042bf87e29db089c0de0d3757499e
Wadzajna <47401405+Wadzajna@users.noreply.github.com>
2022-11-10 20:05:20 +0100

    UploadModelViews

---
+++ Models/IconSelectListItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Services.Models
+{
+    public class IconSelectListItem : SelectListItem
+    {
+        public int? Id { get; set; }
+
+        public string? PreviewImageUrl { get; set; }
+
+        public string? Icon { get; set; }
+
+        public string? Class { get; set; }
+    }
+}
