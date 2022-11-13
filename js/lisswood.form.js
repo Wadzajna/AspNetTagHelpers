2612c72c843f894211fe3c9a4ca2bddeedc28658
Wadzajna <47401405+Wadzajna@users.noreply.github.com>
2022-11-13 11:43:13 +0100

    Add files via upload

---
+++ js/lisswood.form.js
@@ -0,0 +1,135 @@
+﻿$(document).ready(function () {
+
+    //Form.initSubmitAddProductBtn();
+    Form.initGetAddProductPartialViewBtn();
+});
+
+
+var Form = {
+
+    //// Register click button - add product
+    initGetAddProductPartialViewBtn: function () {
+
+        $(document).on("click",
+            "#AddAnotherProductBtn",
+            function () {
+                // Submit product
+                Form.getAddProductPartialView();
+            });
+
+    },
+
+
+    //// Register click button - add product
+    //initSubmitAddProductBtn: function () {
+
+    //    $(document).on("click",
+    //        "#submitProductBtn",
+    //        function () {
+
+    //            var btn = $(this);
+
+    //            var formvalid = $("form").valid();
+
+    //            console.log(formvalid, "formvalid");
+
+    //            // TODO: Make product json object
+    //            var product = {
+    //                Name: $('#AddProduct_Name').val(),
+    //                Description: $('#AddProduct_Description').val(),
+    //                ProductFrameColor_Id: $('input[name="AddProduct.ProductFrameColor_Id"]:checked').val(),
+    //                ProductSize_Id: $('input[name="AddProduct.ProductSize_Id"]:checked').val(),
+    //                ProductLayout_Id: $('input[name="AddProduct.ProductLayout_Id"]:checked').val(),
+    //            }
+
+    //            var data = {
+    //                Name: "",
+    //                AddProduct: product
+    //            }
+
+    //            // Submit product
+    //            Form.submitProduct(data);
+    //        });
+
+    //},
+
+
+    //// Odešle product na server
+    //submitProduct: function (data) {
+
+    //    console.log(data);
+
+    //    $.ajax({
+    //        url: "/Home/AddProductToSession",
+    //        type: "POST",
+    //        data: { data: data },
+    //        success: function (result) {
+
+    //            console.log(result);
+
+    //        },
+    //        error: function (result) {
+
+    //        }
+    //    });
+
+    //},
+
+    // Product added
+    addProductOnSuccess: function (response) {
+        $('#AddProductContainer').html("");
+
+        // Update product count
+        console.log(response);
+        $('#ProductCount').val(response.productCount);
+
+        Form.getProductsPartialView();
+    },
+
+    addProductOnFailure: function () {
+        return;
+    },
+
+    addProductOnComplete: function () {
+        return;
+    },
+
+    // Vrací seznam přidaných produktů
+    getProductsPartialView: function () {
+
+        //GetProductPartialView
+        var key = $('#orderForm #OrderKey').val();
+        console.log(key);
+
+        $.ajax({
+            url: "/Home/GetProductsPartialView",
+            type: "POST",
+            data: { orderkey: key },
+            success: function (result) {
+                $('#Products').html(result);
+            },
+            error: function (result) {
+
+            }
+        });
+    },
+
+    // Vrací nový formulář na přidání nového produktu
+    getAddProductPartialView() {
+        //GetProductPartialView
+        var key = $('#orderForm #OrderKey').val();
+        console.log(key);
+
+        $.ajax({
+            url: "/Home/GetAddProductPartialView",
+            type: "POST",
+            data: { orderkey: key },
+            success: function (result) {
+                $('#AddProductContainer').html(result);
+            },
+            error: function (result) {
+
+            }
+        });
+    }
+}
---
+++ js/liswood.global.js
@@ -0,0 +1,106 @@
+﻿//############################################### GLOBAL FUNKCE, Dostupné přes Global. ###########################################
+
+var Global = {
+    //inicializuje všechny selectBoxy jako multiselectboxy (PRO DATATABLES)
+    initMultiSelectBox: function (indexyMultiSelectu) {
+        for (var i = 0; i < indexyMultiSelectu.length; i++) {
+            var id = "#SEARCH_" + indexyMultiSelectu[i] + "_pom";
+
+            var s = $(id).multiselect({
+                menuWidth: 350,
+                height: 350,
+                classes: "NORMALNI_MULTI"
+            });
+
+            $(".ui-multiselect-none").trigger("click");
+
+            $(document).on("change", id, function () {
+
+                var pomId = $(this).attr("id");
+                var cislo = pomId.substr(0, pomId.length - 4);
+                var form = $("#" + cislo);
+                var idMenu = "#multiselect_" + pomId + "_VALUES";
+                var hodnota = "";
+
+                $(idMenu).find("input[aria-selected='true']").each(function () {
+                    var h = $(this).attr("value");
+                    hodnota = hodnota + ";" + h;
+                });
+
+                form.val(hodnota);
+                form.trigger("change");
+            });
+
+            //$(id).on("change", function () {
+
+            //});
+        }
+        return;
+    },
+
+    //Nastavý vybrané preselect volby v multiselectBoxech (PRO DATATABLES)
+    nastavPreselectMultiBox: function (boxId, hodnoty) {
+        var fo = $(boxId);
+        var elementy = fo.children("option");
+        elementy.removeAttr("preSelected");
+        var pravda = false;
+        $(hodnoty).each(function () {
+            var hodnota = this;
+            if ((hodnota.length < 1)) return;
+            var hodnotaInt = parseInt(hodnota);
+
+            elementy.each(function () {
+                var element = $(this);
+                var val = parseInt($(this).attr("value"));
+                if (val === hodnotaInt) {
+                    element.attr("preSelected", "true");
+                    pravda = true;
+                }
+            });
+        });
+
+        if (pravda) {
+            $(boxId).multiselect("refresh2");
+        }
+    },
+    //obsahuje INDEX (PRO DATATABLES)
+    containsIndexyMultiSelectu: function (a, obj) {
+        for (var i = 0; i < a.length; i++) {
+            if (a[i] === obj) {
+                return true;
+            }
+        }
+        return false;
+    },
+    // Inicializuje klik na tlačítko reset filtry pro základní použití
+    initResetButton: function (pocetInputu, dt) {
+        $("#resetFiltrButton").on("click",
+            function (e) {
+
+                e.preventDefault();
+                $(".ui-multiselect-none").trigger("click");
+
+                for (var i = 0; i <= pocetInputu; i++) {
+                    console.log($("#SEARCH_" + i));
+                    $("#SEARCH_" + i).val("");
+                    $("#SEARCH_" + i + "_pom").val("");
+                    dt.column(i).search("");
+                }
+
+                for (var x = 0; x <= pocetInputu; x++) {
+                    $("#SEARCH_" + x).trigger("change");
+                    $("#SEARCH_" + x).trigger("keyup");
+                }
+
+                return;
+            });
+    },
+
+    // Vrací aktuálně nastavenou kultůru
+    getCurrentCultureName: function () {
+        //Ask ASP.NET what culture we prefer, because we stuck it in a meta tag
+        var data = $("meta[name='accept-language']").attr("content");
+        //Tell jQuery to figure it out also on the client side.
+        return data;
+    }
+}
